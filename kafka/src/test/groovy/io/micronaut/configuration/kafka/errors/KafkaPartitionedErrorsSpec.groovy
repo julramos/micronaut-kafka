26c66f1d2b1bbba432a80f6aa38227d9ac1ff39e
Tim Yates <tim.yates@gmail.com>
2022-03-23 09:42:39 +0000

    Put tests back

--- kafka/src/test/groovy/io/micronaut/configuration/kafka/errors/KafkaPartitionedErrorsSpec.groovy
+++ kafka/src/test/groovy/io/micronaut/configuration/kafka/errors/KafkaPartitionedErrorsSpec.groovy
@@ -3,8 +3,6 @@ package io.micronaut.configuration.kafka.errors
 import spock.lang.Ignore
 import spock.lang.Stepwise
 
-import java.util.stream.IntStream
-
 @Stepwise
 @Ignore("https://github.com/micronaut-projects/micronaut-kafka/issues/514")
 class KafkaPartitionedErrorsSpec extends KafkaErrorsSpec {
@@ -18,34 +16,6 @@ class KafkaPartitionedErrorsSpec extends KafkaErrorsSpec {
     @Override
     protected Map<String, Object> getConfiguration() {
         return super.getConfiguration() + ['spec.name': KafkaErrorsSpec.class.simpleName]
-    }
-
-    void "should correctly handle error strategies"() {
-        when:
-        IntStream.range(0, 30).forEach { i -> producer.send(UUID.randomUUID(), new TestEvent(i)) }
-
-        then:
-        conditions.eventually {
-            listenerWithErrorStrategyResumeAtNextRecord.exceptions.size() == 1
-            listenerWithErrorStrategyResumeAtNextRecord.failed.size() == 1
-            listenerWithErrorStrategyResumeAtNextRecord.events.size() == 29
-
-            listenerWithErrorStrategyRetryOnError.exceptions.size() == 1
-            listenerWithErrorStrategyRetryOnError.failed.size() == 2 // One retry
-            listenerWithErrorStrategyRetryOnError.events.size() == 29
-
-            listenerWithErrorStrategyRetryOnError10Times.exceptions.size() == 1
-            listenerWithErrorStrategyRetryOnError10Times.failed.size() == 11 // 10 times retry
-            listenerWithErrorStrategyRetryOnError10Times.events.size() == 29
-
-            listenerSyncPerRecordWithErrorStrategyRetryOnError10Times.exceptions.size() == 1
-            listenerSyncPerRecordWithErrorStrategyRetryOnError10Times.failed.size() == 11 // 10 times retry
-            listenerSyncPerRecordWithErrorStrategyRetryOnError10Times.events.size() == 29
-
-            listenerWithErrorStrategyNone.exceptions.size() == 1
-            listenerWithErrorStrategyNone.failed.size() == 1
-            listenerWithErrorStrategyNone.events.stream().anyMatch(e -> e.count == 29)
-        }
     }
 
     void "should be correctly processing in partitions"() {
