15a4d791f39a284e59c9b1d976a8cb4339a28e8a
Tim Yates <tim.yates@gmail.com>
2022-03-23 17:53:38 +0000

    Find a different metric that shows the correct tags

--- kafka/src/test/groovy/io/micronaut/configuration/kafka/AbstractEmbeddedServerSpec.groovy
+++ kafka/src/test/groovy/io/micronaut/configuration/kafka/AbstractEmbeddedServerSpec.groovy
@@ -6,14 +6,20 @@ import org.apache.kafka.clients.admin.AdminClient
 import org.apache.kafka.clients.admin.AdminClientConfig
 import org.apache.kafka.clients.admin.NewTopic
 import org.testcontainers.containers.KafkaContainer
+import org.testcontainers.utility.DockerImageName
 import spock.lang.AutoCleanup
 import spock.lang.Shared
-
-import java.util.stream.Collectors
+import spock.util.environment.OperatingSystem
 
 abstract class AbstractEmbeddedServerSpec extends AbstractKafkaSpec {
 
-    @Shared @AutoCleanup KafkaContainer kafkaContainer = new KafkaContainer()
+    // The confluent kafka image doesn't run on M1 mac at present, so use this (old) image for local dev
+    private static boolean isM1Mac = OperatingSystem.current.macOs && System.getProperty("os.arch") == 'aarch64'
+    private static DockerImageName dockerImageName = isM1Mac ?
+            DockerImageName.parse("kymeric/cp-kafka").asCompatibleSubstituteFor("confluentinc/cp-kafka") :
+            DockerImageName.parse("confluentinc/cp-kafka")
+
+    @Shared @AutoCleanup KafkaContainer kafkaContainer = new KafkaContainer(dockerImageName)
     @Shared @AutoCleanup EmbeddedServer embeddedServer
     @Shared @AutoCleanup ApplicationContext context
 
@@ -28,11 +34,6 @@ abstract class AbstractEmbeddedServerSpec extends AbstractKafkaSpec {
 
     void startKafka() {
         kafkaContainer.start()
-    }
-
-    void cleanupSpec() {
-        context.stop()
-        kafkaContainer.stop()
     }
 
     void createTopic(String name, int numPartitions, int replicationFactor) {
